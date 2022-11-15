b4f6eb972218542a44bea2b5f3063cd380e39b86
Nermin <sehicnermin97@gmail.com>
2022-11-15 22:50:59 +1100

    Override enum toString

--- src/main/java/me/nerminsehic/item34/Operation.java
+++ src/main/java/me/nerminsehic/item34/Operation.java
@@ -1,11 +1,37 @@
 package me.nerminsehic.item34;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 // constant-specific method implementations
 public enum Operation {
-    PLUS { public double apply(double x, double y) { return x + y; }},
-    MINUS { public double apply(double x, double y) { return x - y; }},
-    TIMES { public double apply(double x, double y) { return x * y; }},
-    DIVIDE { public double apply(double x, double y) { return x / y; }};
+    PLUS("+") { public double apply(double x, double y) { return x + y; }},
+    MINUS("-") { public double apply(double x, double y) { return x - y; }},
+    TIMES("*") { public double apply(double x, double y) { return x * y; }},
+    DIVIDE("/") { public double apply(double x, double y) { return x / y; }};
+
+    private final String symbol;
+    private static final Map<String, Operation> stringToEnum =
+            Stream.of(values()).collect(Collectors.toMap(Object::toString, e -> e));
+
+    /*
+    * If you override the toString method, you should also consider writing a fromString method
+    * to translate the custom string representation to thr corresponding enum.
+    * */
+    public static Optional<Operation> fromString(String symbol) {
+        return Optional.ofNullable(stringToEnum.get(symbol));
+    }
+
+    Operation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
 
     public abstract double apply(double x, double y);
 }
