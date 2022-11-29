14ea2c756728faad41154de012399b2a7ea6d8a5
Nermin <sehicnermin97@gmail.com>
2022-11-29 21:35:23 +1100

    Avoiding side-effects in stream functions

---
+++ src/main/java/me/nerminsehic/item46/Streams.java
@@ -0,0 +1,32 @@
+package me.nerminsehic.item46;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Streams {
+
+    // Uses the streams API but not the paradigm - Don't do this!!!
+    public static void badExample() {
+        String file = "file";
+
+        Map<String, Long> freq = new HashMap<>();
+        try(Stream<String> words = new Scanner(file).tokens()) {
+            words.forEach(word -> {
+               freq.merge(word.toLowerCase(), 1L, Long::sum);
+            });
+        }
+    }
+
+    // Proper use of streams to initialize a frequency table
+    public static void goodExample() {
+        String file = "file";
+        Map<String, Long> freq;
+
+        try(Stream<String> words = new Scanner(file).tokens()) {
+            freq = words.collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting()));
+        }
+    }
+}
