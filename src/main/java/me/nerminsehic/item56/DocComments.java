24359701f1dcaeaeebbfc116c29905d59fc30d51
Nermin <sehicnermin97@gmail.com>
2022-12-04 16:21:47 +1100

    @implSpec example

--- src/main/java/me/nerminsehic/item56/DocComments.java
+++ src/main/java/me/nerminsehic/item56/DocComments.java
@@ -13,4 +13,14 @@ public interface DocComments<E> {
      * @throws IndexOutOfBoundsException if the index is out of range ({@code index < 0 || index >= this.size()})
      */
     E get(int index);
+
+    /**
+     * Returns true if this collection is empty.
+     *
+     * @implSpec
+     * This implementation returns {@code this.size() == 0}.
+     *
+     * @return true if this collection is empty
+     */
+    boolean isEmpty();
 }
